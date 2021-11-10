47ea2149df6e90edd53dbe209b95fde585b417e9
Michael Bestas <mkbestas@gmail.com>
2021-11-10 23:36:02 +0200

    Update paths for CalyxOS
    Change-Id: I22542e948437fed3c72f310f655a8eb9522c2efe

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,15 +1,15 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from Calyx
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/calyx/sepolicy/atv/vendor
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/system_ext/private
+   device/calyx/sepolicy/atv/system_ext/private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/system_ext/public
+   device/calyx/sepolicy/atv/system_ext/public
